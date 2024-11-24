a86ac0f6e24fae76039690d418a18970287a25f8
xz-dev <xiangzhedev@gmail.com>
2024-11-24 22:40:08 +0800

    feat(provider/gitlab): parser gitlab upload file from changelog

--- src/websdk/repo/provider/gitlab.rs
+++ src/websdk/repo/provider/gitlab.rs
@@ -5,6 +5,7 @@ use std::collections::HashMap;
 
 use super::super::data::release::*;
 use super::base_provider::*;
+use markdown::{mdast::Node, to_mdast, ParseOptions};
 
 use crate::utils::{
     http::{get, head, http_status_is_ok},
@@ -25,6 +26,66 @@ impl GitLabProvider {
 }
 
 impl BaseProviderExt for GitLabProvider {}
+
+impl GitLabProvider {
+    async fn get_project_id(&self, fin: &FIn<'_>) -> Option<String> {
+        let id_map = fin.data_map.app_data;
+        let api_url = format!(
+            "{}/{}%2F{}",
+            GITLAB_API_URL, id_map["owner"], id_map["repo"]
+        );
+        let api_url = self.replace_proxy_url(fin, &api_url);
+
+        if let Ok(parsed_url) = api_url.parse() {
+            if let Ok(rsp) = get(parsed_url, &HashMap::new()).await {
+                if let Some(body) = rsp.body {
+                    if let Ok(data) = serde_json::from_slice::<HashMap<String, Value>>(&body) {
+                        return Some(data.get("id")?.as_number()?.to_string());
+                    }
+                }
+            }
+        }
+        None
+    }
+
+    fn try_get_download_url_from_changelog(&self, changelog: &str) -> Vec<(String, String)> {
+        let changelog_ast = to_mdast(changelog, &ParseOptions::default());
+        if changelog_ast.is_err() {
+            return vec![];
+        }
+        let changelog_ast = changelog_ast.unwrap();
+
+        fn get_link_text(nodes: &Vec<Node>) -> Vec<(String, String)> {
+            let mut url_map = vec![];
+            for node in nodes {
+                let mut name = String::new();
+                if let Node::Link(link) = node {
+                    for c in &link.children {
+                        if let Node::Text(text) = c {
+                            name.push_str(&text.value);
+                        }
+                    }
+                    url_map.push((name, link.url.to_string()));
+                }
+                if let Some(child) = node.children() {
+                    url_map.extend(get_link_text(child));
+                }
+            }
+            url_map
+        }
+        if let Some(children) = changelog_ast.children() {
+            return get_link_text(children);
+        }
+        vec![]
+    }
+
+    fn fix_download_url(&self, download_url: &str, project_id: &str) -> String {
+        if download_url.starts_with("/uploads/") {
+            return format!("{}/-/project/{}{}", GITLAB_URL, project_id, download_url);
+        }
+        download_url.to_string()
+    }
+}
 
 #[async_trait]
 impl BaseProvider for GitLabProvider {
@@ -77,7 +138,6 @@ impl BaseProvider for GitLabProvider {
                     map
                 };
                 if let Ok(rsp) = get(parsed_url, &header_map).await {
-                    println!("{:?}", rsp);
                     if let Some(content) = rsp.body {
                         rsp_body = Some(content);
                     }
@@ -95,8 +155,7 @@ impl BaseProvider for GitLabProvider {
         }
 
         if let Ok(data) = serde_json::from_slice::<Vec<Value>>(body) {
-            println!("{:?}", data);
-            let release_list = data
+            let mut release_list = data
                 .iter()
                 .filter_map(|json| {
                     let assets_data = match json.get("assets")?.get("links") {
@@ -131,6 +190,16 @@ impl BaseProvider for GitLabProvider {
                         }
                     }
                     let changelog = json.get("description")?.as_str()?.to_string();
+                    let extra_download_url = self.try_get_download_url_from_changelog(&changelog);
+                    let assets_data = assets_data.into_iter().chain(
+                        extra_download_url
+                            .into_iter()
+                            .map(|(k, v)| AssetData {
+                                file_name: k,
+                                file_type: "".to_string(),
+                                download_url: v,
+                            })
+                    ).collect();
                     Some(ReleaseData {
                         version_number: version_number?.to_string(),
                         changelog,
@@ -139,6 +208,20 @@ impl BaseProvider for GitLabProvider {
                     })
                 })
                 .collect::<Vec<ReleaseData>>();
+            let mut project_id = None;
+            for release in release_list.iter_mut() {
+                for asset in release.assets.iter_mut() {
+                    if asset.download_url.starts_with("/uploads/") {
+                        if project_id.is_none() {
+                            project_id = self.get_project_id(fin).await;
+                        }
+                        if let Some(project_id) = &project_id {
+                            asset.download_url =
+                                self.fix_download_url(&asset.download_url, project_id);
+                        }
+                    }
+                }
+            }
             fout = fout.set_data(release_list);
         };
 
@@ -169,8 +252,8 @@ mod tests {
         let proxy_url = format!("{} -> {}", GITLAB_URL, server.url());
         let hub_data = HubDataMap::from([(REVERSE_PROXY, proxy_url.as_str())]);
 
-        let github_provider = GitLabProvider::new();
-        assert!(github_provider
+        let gitlab_provider = GitLabProvider::new();
+        assert!(gitlab_provider
             .check_app_available(&FIn::new_with_frag(&id_map, &hub_data, None))
             .await
             .result
@@ -191,8 +274,8 @@ mod tests {
         let proxy_url = format!("{} -> {}", GITLAB_API_URL, server.url());
         let hub_data = HubDataMap::from([(REVERSE_PROXY, proxy_url.as_str())]);
 
-        let github_provider = GitLabProvider::new();
-        let releases = github_provider
+        let gitlab_provider = GitLabProvider::new();
+        let releases = gitlab_provider
             .get_releases(&FIn::new_with_frag(&id_map, &hub_data, None))
             .await
             .result
@@ -201,6 +284,44 @@ mod tests {
         let release_json =
             fs::read_to_string("tests/files/data/provider_gitlab_release.json").unwrap();
         let releases_saved = serde_json::from_str::<Vec<ReleaseData>>(&release_json).unwrap();
+        assert_eq!(releases, releases_saved)
+    }
+
+    #[tokio::test]
+    async fn test_try_get_download_url_from_changelog_in_release() {
+        let body =
+            fs::read_to_string("tests/files/web/gitlab_api_release_AuroraStore.json").unwrap();
+        let mut server = Server::new_async().await;
+        let _m = server
+            .mock("GET", "/AuroraOSS%2FAuroraStore/releases")
+            .with_status(200)
+            .with_body(body)
+            .create();
+        let project_body =
+            fs::read_to_string("tests/files/web/gitlab_api_project_AuroraStore.json").unwrap();
+        let _m = server
+            .mock("GET", "/AuroraOSS%2FAuroraStore")
+            .with_status(200)
+            .with_body(project_body)
+            .create_async()
+            .await;
+
+        let id_map = AppDataMap::from([("owner", "AuroraOSS"), ("repo", "AuroraStore")]);
+        let proxy_url = format!("{} -> {}", GITLAB_API_URL, server.url());
+        let hub_data = HubDataMap::from([(REVERSE_PROXY, proxy_url.as_str())]);
+
+        let gitlab_provider = GitLabProvider::new();
+        let releases = gitlab_provider
+            .get_releases(&FIn::new_with_frag(&id_map, &hub_data, None))
+            .await
+            .result
+            .unwrap();
+
+        let release_json =
+            fs::read_to_string("tests/files/data/provider_gitlab_release_AuroraStore.json")
+                .unwrap();
+        let releases_saved = serde_json::from_str::<Vec<ReleaseData>>(&release_json).unwrap();
+
         assert_eq!(releases, releases_saved)
     }
 }
